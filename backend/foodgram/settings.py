e9bfa4b35f86f16ea3a1ec5016d1403b413eab4f
AndreyYP <wort912@yandex.ru>
2023-10-08 18:40:51 +0500

    deploy v2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -58,14 +58,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-       'USER': os.getenv('POSTGRES_USER', 'foodgram'),
-       'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-       'HOST': os.getenv('DB_HOST', ''),
-       'PORT': os.getenv('DB_PORT', 5432)
-   }
+            'default': {
+                        'ENGINE': 'django.db.backends.postgresql',
+                        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+                        'USER': os.getenv('POSTGRES_USER', 'foodgram'),
+                        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+                        'HOST': os.getenv('DB_HOST', ''),
+                        'PORT': os.getenv('DB_PORT', 5432)
+            }
 }
 AUTH_PASSWORD_VALIDATORS = [
     {
